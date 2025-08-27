29de5f5cc09ac00624a0ce25e4c27f400e513d78
Robert Machado <robert.20.ven@gmail.com>
2025-08-26 21:07:12 -0300

    ahora tailwind retirado por completo

--- telegrom/eslint.config.mjs
+++ telegrom/eslint.config.mjs
@@ -1,14 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: process.cwd(),
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      // Example: you can add your custom rules here
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
