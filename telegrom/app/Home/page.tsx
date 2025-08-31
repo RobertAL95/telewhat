d83548e045407a18d11d405fd7d4b0e1aaf33328
Robert Machado <robert.20.ven@gmail.com>
2025-08-31 00:20:41 -0300

    reconfig 2

--- telegrom/app/Home/page.tsx
+++ telegrom/app/Home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Portada from '@/components/Portada/Render'
+import Portada from '@/Portada/Render'
 import { Box, useMediaQuery } from '@mui/material'
 
 export default function ChatPage() {
